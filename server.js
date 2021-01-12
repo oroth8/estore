6eeb35e121fedd5acc10ba5b0c8e6cf66efbd49c
Owen Roth <rothowen27@gmail.com>
2021-01-12 12:36:45 -0600

    added db connection and data prep for db

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 dotenv.config();
+
+connectDB();
 
 const App = express();
 
@@ -25,5 +29,6 @@ App.listen(
   PORT,
   console.log(
     `Server running in "${process.env.NODE_ENV}" mode on port ${PORT}`
+      .brightBlue.bold
   )
 );
